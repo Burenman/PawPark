18e1f334981d93d205a09478e62ba59f2c1988b9
Viktor <Burenin777@gmail.com>
2023-10-31 13:29:11 -0400

    Initial Commit

---
+++ PawPark/PawParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawParkApp.swift
+//  PawPark
+//
+//  Created by Viktor on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct PawParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
